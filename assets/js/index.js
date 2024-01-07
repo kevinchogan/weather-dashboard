5906f0d2d4f1f472ad834bdfcac307960b0bc8d6
Kevin Hogan <kchogan@pacbell.net>
2024-01-06 23:52:44 -0800

    first pass current and forecast API calls

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,34 @@
+var apiKey = "d3a388cc2e0c9271a4d6036eef79b90b";
+var cityName = "Redwood+City";
 
+function updateCurrent(data) {
+    console.log(data);
+}
 
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=Redwood+City&appid=d3a388cc2e0c9271a4d6036eef79b90b";
+function updateForecast(data) {
+    console.log(data);
+}
 
-fetch(requestUrl)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
+function getForecastApi(data) {
+  updateCurrent(data);
+  var long = data.coord.lon;
+  var lat = data.coord.lat;
+  var forecastApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
+  fetch(forecastApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(updateForecast);
+}
+
+function getApi() {
+  var currentApi = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(currentApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(getForecastApi);
+}
+
+getApi();
